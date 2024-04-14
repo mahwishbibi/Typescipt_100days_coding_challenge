57ed4a57e120905df0c48d4f726f3fa14c5a8bf5
Hira-Tech-GenAi <hiratech88@gmail.com>
2024-04-15 00:03:38 +0500

    100 days Typescript codind quizes

---
+++ day-07/q20/main.ts
@@ -0,0 +1,17 @@
+// 🚀 **Day 7 Challenge: Start Coding!** 🚀
+
+// **Question 20:** Think of something you could store in an array. For example, you could make a list of mountains, rivers, countries, cities, languages, or anything else you’d like. Write a program that creates a list containing these items.
+
+//Think of something you could store in empty array:
+
+let greatNation: string[] = [];
+
+greatNation.push("IsamicRepublic Pakistan"); 
+greatNation.push("Islamabad"); 
+greatNation.push("Urdu");
+greatNation.push("Indus River"); 
+
+console.log(greatNation);
+
+
+
